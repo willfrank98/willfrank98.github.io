65b787ed9167b9e82a4dd950754ea27e683fe1ac
Will Frank <willfrank98@gmail.com>
2019-01-01 14:22:10 -0700

    more icons, more floating modals

--- scripts.js
+++ scripts.js
@@ -78,28 +78,119 @@ new StickyNavigation();
 // JS for project buttons
 //
 window.onload = function() {
-    let modal = document.getElementById('myModal');
-    let link3 = document.querySelector(".item3")
-    let closeBtn = document.getElementsByClassName("close")[0];
+    //TeamBuilder
+    let teamBuilderModal = document.getElementById('TeamBuilder');
+    let teamBuilderLink = document.querySelector(".item3");
+    let teamBuilderClose = document.querySelector("#TeamBuilder > div > div.modal-header > span");
     
     //function which surpresses the link and opens the modal
-    let clickFunc = function (e) {
-        e.preventDefault();
-        modal.style.display = "block";
-    }
+    let clickFuncTB = function (event) {
+        event.preventDefault();
+        teamBuilderModal.style.display = "block";
+    };
     
     //add the clickFunc to the link
-    link3.addEventListener("click", clickFunc);
+    teamBuilderLink.addEventListener("click", clickFuncTB);
     
     //when the user clicks on <span> (x), close the modal
-    closeBtn.onclick = function () {
-        modal.style.display = "none";
+    teamBuilderClose.onclick = function () {
+        teamBuilderModal.style.display = "none";
+    }
+
+    //AI Art
+    let aiArtModal = document.getElementById("AIArt");
+    let aiArtLink = document.querySelector(".item4");
+    let aiArtClose = document.querySelector("#AIArt > div > div.modal-header > span");
+    
+    let clickFuncAA = function (event) {
+        event.preventDefault();
+        aiArtModal.style.display = "block";
+    };
+    
+    aiArtLink.addEventListener("click", clickFuncAA);
+    
+    aiArtClose.onclick = function () {
+        aiArtModal.style.display = "none";
     }
     
+    //Day Planner
+    let dayPlannerModal = document.getElementById("DayPlanner");
+    let dayPlannerLink = document.querySelector(".item5");
+    let dayPlannerClose = document.querySelector("#DayPlanner > div > div.modal-header > span");
+    
+    let clickFuncDP = function (event) {
+        event.preventDefault();
+        dayPlannerModal.style.display = "block";
+    };
+    
+    dayPlannerLink.addEventListener("click", clickFuncDP);
+    
+    dayPlannerClose.onclick = function () {
+        dayPlannerModal.style.display = "none";
+    }
+
+    //Logic Quest
+    let logicQuestModal = document.getElementById("LogicQuest");
+    let logicQuestLink = document.querySelector(".item6");
+    let logicQuestClose = document.querySelector("#LogicQuest > div > div.modal-header > span");
+    
+    let clickFuncLQ = function (event) {
+        event.preventDefault();
+        logicQuestModal.style.display = "block";
+    };
+    
+    logicQuestLink.addEventListener("click", clickFuncLQ);
+    
+    logicQuestClose.onclick = function () {
+        logicQuestModal.style.display = "none";
+    }
+
+    //QtSE
+    let qtSEModal = document.getElementById("QtSE");
+    let qtSELink = document.querySelector(".item7");
+    let qtSEClose = document.querySelector("#QtSE > div > div.modal-header > span");
+    
+    let clickFuncQS = function (event) {
+        event.preventDefault();
+        qtSEModal.style.display = "block";
+    };
+    
+    qtSELink.addEventListener("click", clickFuncQS);
+    
+    qtSEClose.onclick = function () {
+        qtSEModal.style.display = "none";
+    }
+
+    //Simon
+    let simonModal = document.getElementById("Simon");
+    let simonLink = document.querySelector(".item8");
+    let simonClose = document.querySelector("#Simon > div > div.modal-header > span");
+    
+    let clickFuncSi = function (event) {
+        event.preventDefault();
+        simonModal.style.display = "block";
+    };
+    
+    simonLink.addEventListener("click", clickFuncSi);
+    
+    simonClose.onclick = function () {
+        simonModal.style.display = "none";
+    }
+
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
+        if (event.target == teamBuilderModal ||
+            event.target == aiArtModal ||
+            event.target == dayPlannerModal ||
+            event.target == logicQuestModal ||
+            event.target == qtSEModal ||
+            event.target == simonModal) {
+            teamBuilderModal.style.display = "none";
+            aiArtModal.style.display = "none";
+            dayPlannerModal.style.display = "none";
+            logicQuestModal.style.display = "none";
+            qtSEModal.style.display = "none";
+            simonModal.style.display = "none";
         }
     }
 }
